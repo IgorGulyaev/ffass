a493d55a049c7837a034b27022ac672a8ce964f3
IgorGulyaev <i.gulyaev88@gmail.com>
2016-06-10 12:36:27 +0300

    BDT-153 MV Title on Search page fixes (additional commit)

--- skin/frontend/eco/default/js/jq.js
+++ skin/frontend/eco/default/js/jq.js
@@ -353,12 +353,7 @@ jQuery(document).ready(function ($) {
             $('footer .footer-left').appendTo('.navbar .navbar-footer');
             $('.header-top').prependTo('#navbar').show();
 
-            if ($('body').hasClass('catalog-product-view')) {
-                $('.button-filter').insertAfter('.category-title');
-            }
-            if ($('body').hasClass('catalogsearch-result-index')) {
-                $('.button-filter').insertAfter('.page-title');
-            }
+            $('.catalog-product-view .button-filter, .catalogsearch-result-index .button-filter').insertAfter('.page-title');
 
             /* Accordeons */
             $('body:not(.catalog-product-view,  .checkout-onepage-index, .cms-index-index) .nav.nav-tabs').tabCollapse();
