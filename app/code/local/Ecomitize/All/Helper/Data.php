d19a4e025039da2844bfaa90df4beeea298f2399
Vladimir Prutyan <vladimir@ecomitize.com>
2016-10-11 11:53:04 -0700

    BDT-212 show price changes

--- app/code/local/Ecomitize/All/Helper/Data.php
+++ app/code/local/Ecomitize/All/Helper/Data.php
@@ -32,4 +32,38 @@ class Ecomitize_All_Helper_data extends Mage_Core_Helper_Abstract {
         }
         return $options;
     }
+
+    public function getConfigurableOptionsPriceRange($_product){
+
+        if($_product->getTypeId() == 'simple'){
+            return $_product->getFinalPrice();
+        }
+
+        if($_product->getTypeId() == 'configurable') {
+
+            $block = Mage::app()->getLayout()->createBlock('catalog/product_view_type_configurable');
+            $block->setProduct($_product);
+            $_products = $block->getAllowProducts();
+            $config = json_decode($block->getJsonConfig(), true);
+
+            $basePrice = $_product->getSpecialPrice();
+            if (is_null($basePrice)) {
+                $basePrice = $_product->getFinalPrice();
+            }
+
+            foreach ($_products as $_allowProduct) {
+                $option_price = 0;
+                foreach ($config['attributes'] as $aId => $aValues) {
+                    foreach ($aValues['options'] as $key => $value) {
+                        if ($value['label'] === $_allowProduct->getAttributeText($aValues['code'])) {
+                            $option_price += $value['price'];
+                        }
+                    }
+                }
+                $priceRange[] = $option_price + $basePrice;
+            }
+
+            return $priceRange;
+        }
+    }
 }
